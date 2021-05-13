0755bb757ff8d6880d4119ed358bedc4d805ac0c
supersonic <72787599+SuperSonicss@users.noreply.github.com>
2021-05-13 10:16:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,13 +11,19 @@ bot.on("message",(msg)=>{
 
     coinmarketcap.multi(coins => {
      
-        let btcPrice = coins.get("BTC").price_usd
+        
+       let btcPrice = coins.get("BTC").price_usd
+       let whxPrice = coins.get("WHX").price_usd
         let ethPrice = coins.get("ETH").price_usd
         let eth ="eth";
         let btc = "btc";
+        let whx="whx";
         // type eth reviews
         if (msg.text.toString().toLowerCase().indexOf(eth)===0){
             bot.sendMessage(msg.chat.id,"ETH PRICE NOW :"+ ethPrice,{parse_mode:"Markdown"})
+        }
+         if (msg.text.toString().toLowerCase().indexOf(whx)===0){
+            bot.sendMessage(msg.chat.id,"WHX PRICE NOW :"+ ethPrice,{parse_mode:"Markdown"})
         }
         if (msg.text.toString().toLowerCase().indexOf(btc)===0){
             bot.sendMessage(msg.chat.id,"BTC PRICE NOW :"+ btcPrice,{parse_mode:"Markdown"})
